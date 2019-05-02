6ea56640c64569dd0d1b8c92afe18ffdd03df359
ReeceM <reecestevenmay@gmail.com>
2019-05-02 23:30:20 +0200

    feat: add a readable output to the calls done

--- src/Mocked.php
+++ src/Mocked.php
@@ -9,38 +9,98 @@ use Illuminate\Support\Arr;
  * this is what makes tha fake objects that result from reading un-typed params
  */
 class Mocked {
-    private $basename;
+
+    /**
+     * the base array from the new call
+     */
+    private $base;
+
+    /**
+     * the previous calls to the method
+     */
+    private $previous;
+
+    /**
+     * previous vars set
+     */
     private $vars = [];
-        
-    public function __construct($_basename)
+
+    /**
+     * the combined class list to dump for to string
+     * @var array $trace
+     */
+    private $trace = [];
+
+    /**
+     * 
+     * @param string|array $base the name of the arg/object (buttery biscuit base)
+     * @param mixed $previous the base of the calling class
+     */ 
+    public function __construct($base, $previous = [], $vars = [])
     {
-        $this->basename = $_basename;
+        $this->base     = $base;
+        $this->previous = $previous;
+        $this->vars     = $vars;
+
+        if(is_array($this->base)) {
+            $this->structureCalls();
+        }
+    }
+
+    /**
+     * takes the debug trace and structures the single level call
+     * @todo ensure to implement a call limit on this...
+     */
+    private function structureCalls()
+    {
+        try {
+            $args = Arr::get($this->base[0], 'args', [])[0]; // only one if its a get command
+            // $function = Arr::get($this->base[0], 'function', '');
+            // $type = Arr::get($this->base[0], 'type', '');
+            
+            // merge the preceding calls with this one
+            array_push($this->previous, $args);
+            
+            $this->trace = $this->previous;
+            
+            $this->vars = array_fill_keys($this->trace, null);
+
+        } catch (\Exception $th) {
+            throw $th;
+        }
     }
 
     public function __get($name)
     {
-        return Arr::get($this->vars, $name, $this);
-
-        // return __(implode('.', [
-        //                 'mocked',
-        //                 $this->basename,
-        //                 $name
-        //     ]));
+        return new Mocked(debug_backtrace(false, 1), $this->trace, $this->vars);
     }
 
+    public function __call($name, $arguments)
+    {
+        // return new Mocked()
+    }
+
+    /**
+     * set the value of something inside the class
+     */
     public function __set($name, $value)
     {
         $this->vars[$name] = $value;
-        // return $name . ' ' . $value;
     }
 
+    /**
+     * Return a string of the called object
+     * would be at the end of the whole thing
+     */
     public function __toString()
     {
-        return 'mocked ' . $this->basename;
-        /**
-         * @todo make this return a translation instance for the basename
-         * allows user define-able test vars
-         */
-        return __('mocked.' . $this->basename . '_to_string');
+        // dump($this->vars);
+        
+        // $calledValue = Arr::get($this->vars, implode(".", $this->trace));
+
+        // if($calledValue != null) {
+        //     return implode("->", $this->trace) . ' => ' . $calledValue;
+        // }
+        return implode("->", $this->trace);
     }
 }
