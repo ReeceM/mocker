095ecc6839311dc2ef12dd8b52fb7908a5b9564a
ReeceM <reecestevenmay@gmail.com>
2019-05-03 03:00:51 +0200

    feat: improve tests

--- tests/Feature/ReflectionMockeryTest.php
+++ tests/Feature/ReflectionMockeryTest.php
@@ -22,4 +22,48 @@ class ReflectionMockeryTest extends TestCase
         // run the invoke function to use the instantiated data
         $this->assertSame('data->complex->var->that->is->set->too => Hello World', $instance());
     }
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function reflection_accepts_string_test()
+    {
+        $mock = new ReflectionMockery('Tests\ClassOfTest');
+
+        $class = new \ReflectionClass('Tests\ClassOfTest');
+
+        $instance = $class->newInstanceArgs($mock->all());
+        // run the invoke function to use the instantiated data
+        $this->assertSame('data->complex->var->that->is->set->too => Hello World', $instance());
+    }
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function reflection_gives_magic_get_test()
+    {
+        $mock = new ReflectionMockery('Tests\ClassOfTest');
+        
+        $this->assertInstanceOf(\ReeceM\Mocker\Mocked::class, $mock->data);
+        $this->assertInstanceOf(\ReeceM\Mocker\Mocked::class, $mock->get('data'));
+        $this->assertInstanceOf(\ReeceM\Mocker\Mocked::class, $mock->user);
+        $this->assertInstanceOf(\ReeceM\Mocker\Mocked::class, $mock->get('user'));
+    }
+
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function reflection_fails_on_wrong_data_test()
+    {
+        try { 
+            $mocked = new ReflectionMockery(new \Exception('Nope'));
+        } catch (\Exception $e) {
+            $this->assertTrue(true);
+            return;
+        } 
+    }
 }
