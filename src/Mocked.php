4497ce9b62c3158603eff149d3cf1a8d2ac0651b
ReeceM <reecestevenmay@gmail.com>
2020-06-14 20:58:31 +0200

    fix: fixing issue #6

--- src/Mocked.php
+++ src/Mocked.php
@@ -18,7 +18,8 @@ class Mocked extends \ArrayObject
      *
      * Array related methods
      */
-    use ArrayMagic, ObjectMagic;
+	use ArrayMagic, ObjectMagic;
+
     /**
      * the base array from the new call
      */
@@ -45,25 +46,27 @@ class Mocked extends \ArrayObject
      * the combined class list to dump for to string
      * @var array $trace
      */
-    private $trace = [];
+	private $trace = [];
 
     private static $GET_METHOD = "__get";
     private static $SET_METHOD = "__set";
 
-    /*
+    /**
      * The mocked constructor
+	 *
      * @param string|array $base the name of the arg/object (buttery biscuit base)
      * @param \ReeceM\Mocker\Utils\VarStore $store singleton variable storage
-     * @param mixed $previous the base of the calling class
+     * @param mixed|\ReeceM\Mocker\Mocked $previous the base of the calling class
      */
-    public function __construct($base, VarStore $store, $previous = [])
+    public function __construct($base, VarStore $store = null, array $previous = [])
     {
         $this->previous = $previous;
         $this->store    = $store;
-        $this->base     = $base;
+		$this->base     = $base;
 
         if (is_string($base)) {
-            $this->base     = [['args' => [$base], 'function' => static::$GET_METHOD]];
+			$this->base     = [['args' => [$base], 'function' => static::$GET_METHOD]];
+			$this->store->mocked = $this;
         }
 
         $this->structureMockeryCalls();
@@ -89,37 +92,26 @@ class Mocked extends \ArrayObject
         }
     }
 
-    private function setMockeryVariables($args, $function = null)
+	/**
+	 * determine where to place data into the central store
+	 * @param array $args the arguments from the accessors
+	 * @param string|null $function the function action
+	 * @return void
+	 */
+    private function setMockeryVariables(array $args, string $function = null)
     {
 		$memorable = $this->store->memoized;
+		$arrayCall = $this->base[0]["__linking"] ?? null;
 
-		if ($function === self::$SET_METHOD) {
-			Arr::set($memorable, $this->resolvedBase(), $args[1]);
+		if ($function === self::$SET_METHOD || $arrayCall) {
+			if ($args[0] == null) {
+				$memorable[] = $args[1] ?? null;
+			} else {
+				Arr::set($memorable, implode('.', $this->trace), $args[1] ?? null);
+			}
 		}
 
 		$this->store->memoized = $memorable;
-		// if ($value instanceof self) {
-			// $this->store->memoized = array_merge($this->store->memoized, $memorable);
-		// }
-		// Arr::set($memorable, $key, $value)
-        // $memorable[$key] = $value;
-
-    }
-
-	/**
-	 * Get the base of the current object or array that is being queried at trace end
-	 *
-	 * @return mixed
-	 */
-	private function resolvedBase()
-	{
-		return implode('.', $this->trace);
-		// $args = Arr::get($this->base[0], 'args', []);
-		// $function = Arr::get($this->base[0], 'function', '__get');
-
-		return Arr::get($this->store->memoized, implode('.', $this->trace));
-		// ie if $mocked->d->dd->ddd; should return whats at tip of ddd, not d/mocked
-		// Arr::get()
 	}
 
     /**
@@ -142,5 +134,5 @@ class Mocked extends \ArrayObject
     public function __getStore()
     {
         return $this->store->memoized;
-    }
+	}
 }
