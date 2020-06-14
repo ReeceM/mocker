f6136b894c42c52f1ca8bd77bd41191aae5a5315
ReeceM <reecestevenmay@gmail.com>
2020-06-14 20:57:18 +0200

    fix: array multi dimensional and returns

--- src/Traits/ArrayMagic.php
+++ src/Traits/ArrayMagic.php
@@ -7,67 +7,55 @@ use Illuminate\Support\Arr;
 trait ArrayMagic
 {
 
-    /**
-     * Returns the iterator for loops
-     * @return \ArrayIterator
-     */
-    public function getIterator(): \ArrayIterator
-    {
-        return new \ArrayIterator($this->store->memoized);
-    }
-
-    /**
-     * sets a value in the array object using the offset
-     * @param mixed $offset key of the array
-     * @param mixed $value the value to insert at offset
-     * @return void|self
-     */
-    public function offsetSet($offset, $value)
-    {
-		/**
-		 * If a user pushes $mocked[] = [], the offset is null
-		 * Should push onto the previous called value
-		 */
-		if (is_null($offset)) {
-			// $memoized = $this->store->memoized;
-			// Arr::set()
-			$this->store->memoized[] = $value;
-			return $this->offsetGet($offset);
-		}
-
-		return new self($this->arrayTrace(), $this->store, $this->trace);
-    }
-
-    /**
-     * This gets the value at the offset, if it is not set it will set and return a new instance
-     *
-     * @param mixed $offset the offset to get
-     * @return self
-     */
-	public function offsetGet($offset)
+	/**
+	 * Returns the iterator for loops
+	 * @return \ArrayIterator
+	 */
+	public function getIterator(): \ArrayIterator
 	{
-		return new self($this->arrayTrace(), $this->store, $this->trace);
-		// return Arr::has($this->store->memoized, implode('.', $this->trace) . '.' . $offset )
-		// 	? Arr::get($this->store->memoized, implode('.', $this->trace) . '.' . $offset)
-		// 	: new self($this->arrayTrace(), $this->store, $this->trace);
+		return new \ArrayIterator($this->store->memoized);
 	}
 
-    /**
-     * checks if the key exists in the array
-     *  @return bool
-     */
-    public function offsetExists($offset)
-    {
-        return isset($this->store->memoized[$offset]);
-    }
-    /**
-     * unsets a key in array/object
-     *  @return void
-     */
-    public function offsetUnset($offset)
-    {
-        unset($this->store->memoized[$offset]);
-    }
+	/**
+	 * sets a value in the array object using the offset
+	 * @param mixed $offset key of the array
+	 * @param mixed $value the value to insert at offset
+	 * @return void|self
+	 */
+	public function offsetSet($offset, $value)
+	{
+		return new self($this->arrayTrace(), $this->store, $this->trace);
+	}
+
+	/**
+	 * This gets the value at the offset, if it is not set it will set and return a new instance
+	 *
+	 * @param mixed $offset the offset to get
+	 * @return self
+	 */
+	public function offsetGet($offset)
+	{
+		return Arr::has($this->store->memoized, implode('.', $this->trace) . '.' . $offset)
+			? Arr::get($this->store->memoized, implode('.', $this->trace) . '.' . $offset)
+			: new self($this->arrayTrace(), $this->store, $this->trace);
+	}
+
+	/**
+	 * checks if the key exists in the array
+	 *  @return bool
+	 */
+	public function offsetExists($offset)
+	{
+		return Arr::has($this->store->memoized, implode('.', $this->trace));
+	}
+	/**
+	 * unsets a key in array/object
+	 *  @return void
+	 */
+	public function offsetUnset($offset)
+	{
+		Arr::pull($this->store->memoized, implode('.', $this->trace));
+	}
 
 	/**
 	 * Back trace the calls in the array as we are getting it through two functions.
@@ -75,16 +63,16 @@ trait ArrayMagic
 	 * @param int $depth how far back to go
 	 * @return array
 	 */
-    private function arrayTrace($depth = 2): array
-    {
+	private function arrayTrace($depth = 2): array
+	{
 		$selfTrace = debug_backtrace(false, $depth);
 		$target = ($depth - 1) >= 0 ? $depth - 1 : 0;
 
-        return array([
-            'function'  => $selfTrace[$target]['function'] == 'offsetGet' ? '__get' : '__set',
-            'args'      => $selfTrace[$target]['args'] ?? [null],
+		return array([
+			'function'  => $selfTrace[$target]['function'] == 'offsetGet' ? '__get' : '__set',
+			'args'      => $selfTrace[$target]['args'] ?? [null],
 			'type'      => $selfTrace[$target]['type'] ?? '->',
 			'__linking' => 'arr_mag',
-        ]);
-    }
+		]);
+	}
 }
